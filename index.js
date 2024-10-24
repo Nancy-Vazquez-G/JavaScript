1609a21a624f52714d3ffbbc968759b1fa16fd66
Nancy Vazquez <nancy.vazquez.prog@outlook.com>
2024-10-24 17:24:01 -0600

    entrega 2

--- index.js
+++ index.js
@@ -1,35 +1,94 @@
 
-function mostrarCursos() {
-    let catalogo = "Catálogo de cursos: \n";
-    catalogo += "1. Noviazgo y sexualidad \n";
-    catalogo += "2. Inteligencia emocional \n";
-    catalogo += "3. Mi yo social \n";
-    catalogo += "¿Quieres conocer nuestras propociones?\n"
-    return catalogo;
+const cursos=[
+    {id:1, curso: "Noviazgo y sexualidad", precio: "1,500"},
+    {id:2, curso: "Inteligencia emocional", precio: "1,500"},
+    {id:3, curso: "Mi yo social", precio: "1,500"},
+    {id:4, curso: "Administración del tiempod", precio: "1,500"},
+    {id:5, curso: "Finanzas sanas", precio: "1,500"},
+    {id:6, curso: "El costo de tener la razón", precio: "1,500"},
+];
+
+let flag_fin = true;
+let total = 0.0;
+let showCursos = "";
+let cantidad = 0;
+let carrito =[];
+
+for (p in cursos) {
+    showCursos =
+        showCursos + `${cursos[p].id}. ${cursos[p].curso} = $ ${cursos[p].precio} \n`;
 }
 
-function elegirCurso() {
-    let total = 0;
-    let seguirEligiendo = true;
+function init() {
+	while (flag_fin) {
+		let id = parseInt(
+			prompt(
+				"Catalogo de cursos\n" + showCursos + "Ingresa el id del cruso"
+			)
+		);
 
-    while (seguirEligiendo) {
-        let catalogo = mostrarCursos();
-        let seleccion =prompt(
-            "¿A qué curso quieres asistir? Ingresa el número o escribe SALIR"
-        );
+		let cursoSeleccionado = buscarCurso(id);
+		console.log(cursoSeleccionado);
 
-        if (seleccion === null || seleccion === "") {
-            alert("Dato incorrecto");
-            continue;
-        }
+		if (cursoSeleccionado.hasOwnProperty("id")) {
+			cantidad += 1;
+			carrito.push(cursoSeleccionado);
 
-        if (seleccion === SALIR) {
-            break;
-        }
+			let validarPromocion = calcularPromocion(cantidad, carrito);
 
-        if (isNaN(seleccion) || seleccion<1 || seleccion > 3) {
-            alert("Dato incorrecto");
-            continue;
-        }
-    }
+			if (validarPromocion.promocion) {
+				alert("Tu carrito tiene promoción");
+			}
+		} else {
+			console.log("El id del curso seleccionado no existe");
+		}
+
+		flag_fin = confirm("¿Deseas adquirir otro curso?");
+	}
 }
+
+function calcularPromocion(cantidad, carrito) {
+	console.log("carrito", carrito);
+	console.log("cantidad", cantidad);
+
+	let total = 0.0;
+	let nuevoCarrito = carrito;
+	let tienePromocion = false;
+	let resultado = {};
+
+	if (cantidad >= 3) {
+		tienePromocion = true;
+		nuevoCarrito.reduce((a, b) => a + b.precio, 0);
+		console.log(nuevoCarrito);
+	}
+
+	if (tienePromocion) {
+		resultado["promocion"] = true;
+		
+		console.log(total);
+	} else {
+		resultado["promocion"] = false;
+		total = nuevoCarrito.map((x) => x.precio).reduce((a, b) => parseFloat(a) + parseFloat(b));
+		console.log(total);
+	}
+
+	return resultado;
+}
+
+function buscarCurso(id) {
+	let detalleCurso = {};
+
+	for (p in cursos) {
+		if (id == cursos[p].id) {
+			detalleCurso = {
+				id: id,
+				cruso: cursos[p].cruso,
+				precio: cursos[p].precio,
+			};
+		}
+	}
+
+	return detalleCurso;
+}
+
+init();
